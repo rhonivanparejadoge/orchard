69e26dee0f2e258317cae427a7a2501de9ee4ade
Rhon Ivan Lewis Pareja <rhonivan.pareja@gmail.com>
2024-03-06 21:14:06 +0800

    Create three up component and make the component responsive

---
+++ src/components/3upComponent/3upComponent.js
@@ -0,0 +1,15 @@
+import { $ } from '../../js/helpers/selectors';
+
+let SELECTOR = '.three-up-component';
+
+class ThreeUpComponent {
+  constructor() {
+    if (! $(SELECTOR)) {
+      return false;
+    }
+
+    //Your code below here
+  }
+}
+
+export default ThreeUpComponent;
--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,13 @@ import '../scss/app.scss';
 
 /* Imports */
 import ImageAndText from '../components/ImageAndText/ImageAndText';
+import ThreeUpComponent from '../components/3upComponent/3upComponent';
 
 // Add a map for each component class
 const classes = new Map(
   [
     ['ImageAndText', ImageAndText],
+    ['ThreeUpComponent', ThreeUpComponent]
   ]
 );
 
